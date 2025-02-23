9bd33a13025a59c4ed2cc95818a7b9f7dfb82106
BekMart <bekmart1989@gmail.com>
2025-02-23 13:31:46 +0000

    Create app with functional component

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FunctionalGreeting />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Vanilla JavaScript
+// function FunctionalGreeting() {
+//   return <h1>Hello, Functional Component!</h1>;
+// }
+
+// Arrow function (same as above) *prefered method*
+const FunctionalGreeting = () => <h1>Hello, Functional Component!</h1>;
+
+export default FunctionalGreeting;
